e3cdedca07d2d9b8e72e9099a671648d6d6ed579
Luiz <luiz.fatec2008@gmail.com>
2024-05-06 21:18:26 -0300

    adição de novos arquivos

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,72 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(SuperApp());
+  runApp(const MaterialApp(
+    home: HomePage(),
+  ));
 }
 
-class SuperApp extends StatelessWidget {
+class CatalogoItem {
+  final String title;
+  final String description;
+
+  CatalogoItem(this.title, this.description);
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _index = 0;
+
+  Widget? _switcherBody() {
+    switch (_index) {
+      case 0:
+        return Container(child: const Center(child: Text("CATALOGO")));
+      case 1:
+        return Container(child: const Center(child: Text("RESERVA")));
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'BIBLIOTECA COMUNITÁRIA UNICEU ALVARENGA',
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text('BIBLIOTECA COMUNITÁRIA UNICEU ALVARENGA'),
-          ),
-          backgroundColor: Color(0xffc6d7c6),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'Tela 1',
-                  style: TextStyle(fontSize: 40),
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xff82ba86),
-                    shadowColor: Color(0xffdb3208),
-                    elevation: 3,
-                  ),
-                  onPressed: () {
-                    print('Botão pressionado');
-                  },
-                  child: const Text('Botão'),
-                ),
-              ],
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text("BIBLIOTECA COMUNITÁRIA \nUNICEU ALVARENGA")),
+      body: _switcherBody(),
+      drawer: Drawer(
+        child: ListView(padding: EdgeInsets.zero, children: <Widget>[
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 177, 202, 226),
             ),
-          )),
+            child: Text('BIBLIOTECA COMUNITÁRIA \nUNICEU ALVARENGA'),
+          ),
+          ListTile(
+            title: const Text('CATALOGO'),
+            onTap: () {
+              setState(() {
+                _index = 0;
+              });
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: const Text('RESERVA'),
+            onTap: () {
+              setState(() {
+                _index = 1;
+              });
+              Navigator.pop(context);
+            },
+          ),
+        ]),
+      ),
     );
   }
 }
